e553d84e1977369ae59be828ebf9353e5e2bc969
jaybamania <90679000+jaykumarbamania@users.noreply.github.com>
2026-01-29 00:02:41 +0530

    added order aggregate with state machine

---
+++ src/main/java/com/project/oms/order/domain/Order.java
@@ -0,0 +1,40 @@
+package com.project.oms.order.domain;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public class Order {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false)
+    private UUID userId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private OrderStatus status;
+
+    @Column(nullable = false)
+    private BigDecimal totalAmount;
+
+    @Version
+    private Long version;
+
+    public Order(UUID userId, BigDecimal totalAmount) {
+        this.userId = userId;
+        this.totalAmount = totalAmount;
+        this.status = OrderStatus.CREATED;
+    }
+
+    public void transitionTo(OrderStatus nextStatus) {
+        if(!OrderStateMachine.isValidTransition(this.status,nextStatus)) {
+            throw new IllegalStateException(
+                    "Invalid order state transition: " + status + " -> " + nextStatus
+            );
+        }
+        this.status = nextStatus;
+    }
+}
